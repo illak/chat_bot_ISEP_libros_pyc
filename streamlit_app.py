e4305aa905d9d6c427db816cd2c578c1b51f15e9
Illak Zapata <i.zapata1989@gmail.com>
2024-05-03 14:14:00 -0300

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,24 @@
 import os
-from PyPDF2 import PdfReader
-import pdfplumber
 
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PDFPlumberLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 from langchain_core.output_parsers import StrOutputParser
 
 from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
+
+from langchain_core.runnables import RunnablePassthrough
+
 
 SAFETY_SETTINGS = {
     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
@@ -32,54 +33,52 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+
 # read all pdf files and return text
+@st.cache_resource
+def load_pdfs():
 
+    directory = "libros_pyc"
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=400) # chunk_overlap 1000
 
-            # Limpieza
-            text = re.sub(r" +", " ", text)
-            text = re.sub(r'\n', ' ', text)
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
 
-    return text
-
-def load_pdf_text():
-    text = ""
-
-    directory = 'libros_pyc'
- 
     # iterate over files in
     # that directory
-    for filename in os.listdir(directory):
+    for index, filename in enumerate(os.listdir(directory)):
         f = os.path.join(directory, filename)
         # checking if it is a file
         if os.path.isfile(f):
 
             print(f)
+            loader = PDFPlumberLoader(f)
 
-            with pdfplumber.open(f) as pdf:
-                print(len(pdf.pages))
-                for page in pdf.pages:
-                    text += page.extract_text()
+            data = loader.load()
 
-                    # Limpieza
-                    #text = re.sub(r" +", " ", text)
-                    text = re.sub(r'\n', ' ', text)
+            print("==========================")
+            print(data[11])
+            print(data[12])
+            print("==========================")
 
-    return text
+            splits = splitter.split_documents(data)
+
+            #vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings, persist_directory="./chroma_db")
+            if index == 0:
+                vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
+            else:
+                db_i = FAISS.from_documents(documents=splits, embedding=embeddings)
+                vectorstore.merge_from(db_i)
+
+            print(vectorstore.index.ntotal)
 
 
-# split text into chunks
-def get_text_chunks(text):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1500, chunk_overlap=200) # chunk_overlap 1000
-    chunks = splitter.split_text(text)
-    return chunks  # list of strings
+    retriever = vectorstore.as_retriever()
+    # Devolvemos retriever
+    return retriever
 
 
 # get embeddings for each chunk
@@ -90,18 +89,26 @@ def get_vector_store(chunks):
     vector_store.save_local("faiss_index")
 
 
-def get_conversational_chain():
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-    prompt_template = """
-    Responder a la pregunta del usuario lo más detallademente posible. Si la respuesta no se encuentra
-    en el contexto provisto responda que no encontró información en dicho contexto, no devuelva una respuesta incorrecta.\n\n
+def debug_prompt(x):
+    print(x)
+    return x
 
-    Contexto: {context}\n\n
 
-    Pregunta: {question}\n
+# testing
+def get_conversational_chain(retriever):
 
-    Respuesta:
-    """
+    prompt_template = """Responder a la pregunta del usuario lo más detallademente posible. Si la respuesta no se encuentra
+en el contexto provisto responda que no encontró información en dicho contexto, no devuelva una respuesta incorrecta.\n\n
+
+Contexto: {context}\n\n
+
+Pregunta: {question}\n
+
+Respuesta:
+"""
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
@@ -113,9 +120,15 @@ def get_conversational_chain():
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
     
-    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    #chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
 
-    #chain = prompt | model | StrOutputParser()
+    chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt 
+        | debug_prompt
+        | model 
+        | StrOutputParser()
+    )
 
     return chain
 
@@ -125,38 +138,14 @@ def clear_chat_history():
         {"role": "assistant", "content": "Haga preguntas..."}]
 
 
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001")  # type: ignore
+def user_input(user_question, retriever):
 
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    chain = get_conversational_chain(retriever)
+
+    response = chain.invoke(user_question)
     
-    docs = new_db.similarity_search(user_question, k = 6)
-
-    print(len(docs))
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
-    print(docs)
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
-
-    chain = get_conversational_chain()
-    
-    #response = chain.stream({
-    #        "context": docs, 
-    #        "question": user_question
-    #     })
-
-    response = chain(
-        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
-
     print(response)
     return response
-
-@st.cache_resource
-def initialize_vector_db():
-    raw_text = load_pdf_text()
-    text_chunks = get_text_chunks(raw_text)
-    get_vector_store(text_chunks)
-    st.success("Listo!")
 
 
 def main(): 
@@ -167,8 +156,7 @@ def main():
 
     # Cargamos los libros de PYC al INICIO
     with st.spinner("Procesando PDFs..."):
-        initialize_vector_db()
-
+        retriever = load_pdfs()
 
     # Sidebar for uploading PDF files
     _ = """with st.sidebar:
@@ -209,12 +197,13 @@ def main():
         with st.chat_message("assistant"):
             with st.spinner("Estoy pensando..."):
                 #response = st.write_stream(user_input(prompt))
-                response = user_input(prompt)
+                response = user_input(prompt, retriever)
                 placeholder = st.empty()
-                full_response = ''
-                for item in response['output_text']:
-                    full_response += item
-                    placeholder.markdown(full_response)
+                full_response = response
+                #full_response = ''
+                #for item in response['output_text']:
+                #    full_response += item
+                #    placeholder.markdown(full_response)
                 placeholder.markdown(full_response)
                 
         if response is not None:
